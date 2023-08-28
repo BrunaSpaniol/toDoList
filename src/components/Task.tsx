03270b249bad7b165379066d50e5f1ef38347f76
Bruna Spaniol <bruna.spaniol@gmail.com>
2023-08-28 18:43:43 -0300

    feat: funções para renderização da lógica da página

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,15 +1,36 @@
 import { Trash } from "@phosphor-icons/react";
 import styles from "./Task.module.css";
 
-export function Task() {
-  return (
-    <section className={styles.task}>
-      <input type="checkbox"></input>
-      <p>Aprender Francês</p>
+interface TaskProps {
+  id: string;
+  isCheck: boolean;
+  message: string;
+  handleDelete: (id: string) => void;
+  handleDoneTask: (id: string, checked: boolean) => void;
+}
 
-      <button className={styles.delete}>
-        <Trash size={16} weight="bold" />
-      </button>
+export function Task({
+  id,
+  isCheck,
+  message,
+  handleDelete,
+  handleDoneTask,
+}: TaskProps) {
+  return (
+    <section className={styles.taskContainer}>
+      <div className={styles.task}>
+        <input
+          type="checkbox"
+          checked={isCheck}
+          onClick={(event) => handleDoneTask(id, event.currentTarget.checked)}
+        ></input>
+
+        <p>{message}</p>
+
+        <button className={styles.delete} onClick={() => handleDelete(id)}>
+          <Trash size={16} weight="bold" />
+        </button>
+      </div>
     </section>
   );
 }
